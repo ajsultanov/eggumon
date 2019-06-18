036af6508a0a9bca328297472255e90f043e48dc
Adam Sultanov <adam.sultanov@gmail.com>
2019-06-18 13:54:33 -0400

    controller update

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -31,7 +31,7 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:id, :name)
+    params.permit(:name)
   end
 
   def find_user
