8cd44101a8abbc20ca6591aa807bd2958252b032
Adam Sultanov <adam.sultanov@gmail.com>
2019-06-18 15:03:05 -0400

    merge

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,6 @@
 
-User.create(id: 2, name: "Bob")
-User.create(id: 3, name: "Charlie")
-User.create(id: 4, name: "Dave")
-User.create(id: 5, name: "Eve")
+User.create(id: 1, name: "adam")
+User.create(id: 2, name: "christian")
 
-Pet.create(id: 1, name: "Pikachu", user_id: 1)
-Pet.create(id: 2, name: "Bulbasaur", user_id: 2)
-Pet.create(id: 3, name: "Charizard", user_id: 2)
-Pet.create(id: 4, name: "Squirtle", user_id: 3)
-Pet.create(id: 5, name: "Mew", user_id: 3)
-Pet.create(id: 6, name: "Mewtoo", user_id: 3)
+Pet.create(id: 1, name: "Pikachu", user_id: 1, img: "pet32.png")
+Pet.create(id: 2, name: "Bulbasaur", user_id: 2, img: "pet32.png")
